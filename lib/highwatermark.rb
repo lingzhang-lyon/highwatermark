681438b3cdbfd18a63d03b0b7b45b53211731c64
Ling Zhang <zhangling.ice@gmail.com>
2015-04-16 11:28:07 -0700

    add test_state_file, modify highwartermark.rb to add "last_records", clean code

--- lib/highwatermark.rb
+++ lib/highwatermark.rb
@@ -1,141 +1,76 @@
 require "highwatermark/version"
 
 module Highwatermark
-    # Your code goes here...
-    class StateStore
-      def initialize(path)
-        require 'yaml'
-        @path = path
-        if File.exists?(@path)
-          @data = YAML.load_file(@path)
-          if @data == false || @data == []
-            @data = {}
-          elsif !@data.is_a?(Hash)
-            raise "Spectrum :: ConfigError state_file on #{@path.inspect} is invalid"
-          end
-        else
-          @data = {}
-        end
-      end
+    class HighWaterMark
+    	def initialize(path,state_type,tag)
+    		# path: is th file path for store state or the redis configure
+    		# state_type: could be <redis/file/memory>
+    		# tag: is the tag that will be used in state file or redis 
 
-      def last_records
-        @data['last_records'] ||= {}
-      end
 
-      def update!
-        File.open(@path, 'w') {|f|
-          f.write YAML.dump(@data)
-        }
-      end
-    end
+    		require 'yaml'
+    		@path = path
+    		@state_type = state_type
+			  @tag = tag
 
-    class MemoryStateStore
-      def initialize
-        @data = {}
-      end
-      
-      def last_records
-        @data['last_records'] ||= {}
-      end
-      
-      def update!
-      end
-    end
+    		@data = {}
+    		if @state_type =='file'
+    			if File.exists?(path)
+  					@data = YAML.load_file(path)
+  					if @data == false || @data == []
+  						# this happens if an users created an empty file accidentally
+  						@data = {}
+  					elsif !@data.is_a?(Hash)
+  						raise "state_file on #{@path.inspect} is invalid"
+  					end
+  				else
+  					@data = {}
+  				end
+  			elsif @state_type =='memory'
+  				@data = {}
+  			elsif @state_type =='redis'
+  				require 'redis'
+  				$redis = if File.exists?(path)
+  					redis_config = YAML.load_file(path)
+  					# Connect to Redis using the redis_config host and port
+  					if path
+  					    begin
+  					        pp "In redis #{path} Host #{redis_config['host']} port #{redis_config['port']}"
+  				  			$redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+  						rescue Exception => e
+  							$log.info e.message
+  		                	$log.info e.backtrace.inspect
+  					    end
+  					end
+  				else
+  			  		Redis.new
+  				end
+      		@data = {}
+      	end # end of checking @state_type
+        @data['last_records'] = {}
+      end # end of intitialize
 
-    # from appdynamics
-    # Class statestore
-    class StateStore
-      def initialize(path,tag)
-        require 'yaml'
-        @path = path
-        if File.exists?(@path)
-          @data = YAML.load_file(@path)
-          if @data == false || @data == []
-            # this happens if an users created an empty file accidentally
-            @data = {}
-          elsif !@data.is_a?(Hash)
-            raise "state_file on #{@path.inspect} is invalid"
-          end
-        else
-          @data = {}
-        end
-      end
+	    def last_records()
+          # return @data[@tag]
+          return @data['last_records'][@tag]
+  		end
 
-      def last_records(tag=nil)
-        return @data[tag]
-        #@data['last_records'] ||= {}
-      end
+  		def update_records(time)
+        # @data[@tag] = time
+  			@data['last_records'][@tag] = time
+  			# $log.info  @data
+  			File.open(@path, 'w') {|f|
+  			  f.write YAML.dump(@data)
+  			}
+  		end
 
-      def update_records(time, tag=nil)
-		@data[tag]=time
-		pp  @data
-        File.open(@path, 'w') {|f|
-          f.write YAML.dump(@data)
-        }
-      end
-    end
+  	end # end of class Highwatermark
 
-    # Class store in memory
-    class MemoryStateStore
-      def initialize
-        @data = {}
-      end
-      
-      def last_records(tag=nil)
-        @data['last_records'] ||= {}
-      end
-      
-      def update_records(time,tag=nil)
-      end
-    end
 
-    # Class store in redis
-    class RedisStateStore
-      state_key = ""
-      def initialize(path,tag)
-        state_key=tag
-		#redis_server = $appsettings['redis_server']
-		#redis_port = $appsettings['redis_port']
-		#redis_spectrum_key = $appsettings['redis_spectrum_key']
-		#####
-      	require 'redis'
-		$redis = if File.exists?(path)
-			redis_config = YAML.load_file(path)
-			# Connect to Redis using the redis_config host and port
-			if path
-			    begin
-			        pp "In redis #{path} Host #{redis_config['host']} port #{redis_config['port']}"
-		  			$redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
-				    rescue Exception => e
-						$log.info e.message
-	                	$log.info e.backtrace.inspect
-			    end
-			end
-		else
-  		Redis.new
-	  end
-        @data = {}
-      end
-      
-      def last_records(tag=nil)
-        begin
-  	   alertStart=$redis.get(tag)
-           return alertStart
-        rescue Exception => e
-	   $log.info e.message
-	   $log.info e.backtrace.inspect
-        end
-      end
-      
-      def update_records(time, tag=nil)
-        begin
-  	   		alertStart=$redis.set(tag,time)
-        rescue Exception => e
-		   $log.info e.message
-		   $log.info e.backtrace.inspect
-        end
-      end
-    end
+
+    		
+
+    
 
 
 end
--- test/test_highwatermark.rb
+++ test/test_highwatermark.rb
@@ -1,4 +1,6 @@
 require 'minitest_helper'
+# require 'highwatermark'
+# require 'minitest/autorun'
 
 class TestHighwatermark < Minitest::Test
   def test_that_it_has_a_version_number
@@ -6,6 +8,24 @@ class TestHighwatermark < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    # assert false
   end
+
+  def test_could_work
+  	path = 'test/test_state_file.conf'
+  	state_type = 'file'
+  	tag = 'testTag'
+
+  	hwm = ::Highwatermark::HighWaterMark.new(path, state_type, tag)
+  	# time = Engine.now
+  	time = 'testTime'
+  	hwm.update_records(time)
+  	
+  	assert_equal time,
+      hwm.last_records()
+  end
+
+
+
+
 end
